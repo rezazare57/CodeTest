ecb60a41e039c6436468feafbc316d3227cd1929
rezazare57 <mreza_zare57@yahoo.com>
2018-01-02 01:21:53 +0800

    added skeleton task application

--- src/main/java/taskapp/application/Main.java
+++ src/main/java/taskapp/application/Main.java
@@ -1,7 +1,10 @@
 package taskapp.application;
+import org.joda.time.LocalTime;
 public class Main{
 	public static void main(String[]args)
 	{
+		LocalTime time=new LocalTime();
+		System.out.print("Local time is :"+time);
 		System.out.print("Hello World Joon");
 	}
 }
